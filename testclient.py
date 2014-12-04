f19fe4eaf87b17eaf57586fc46c5bbd7e514e5f1
guoch <linuxwalkers@163.com>
2014-12-04 17:52:08 +0800

    newserver

--- testclient.py
+++ testclient.py
@@ -1,3 +1,4 @@
+'''
 import socket
 HOST='localhost'
 PORT=50000
@@ -7,3 +8,20 @@ s.sendall('Hello,world')
 data=s.recv(1024)
 s.close()
 print 'Received',repr(data)
+'''
+#-*-coding:utf-8-*- 
+import socket
+import sys
+
+HOST,PORT='localhost',50000
+data=" ".join(sys.argv[1:])
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+try:
+	sock.connect((HOST,PORT))
+	sock.sendall(data+"\n")
+	received=sock.recv(1024)
+finally:
+	sock.close()
+
+print "Sent: {}".format(data)
+print "Received: {}".format(received)
--- testserver.py
+++ testserver.py
@@ -1,3 +1,4 @@
+'''
 import socket
 HOST=''
 PORT=50000
@@ -11,3 +12,46 @@ while 1:
 	if not data:break
 	conn.sendall(data)
 conn.close()
+'''
+#-*-coding:utf-8-*- 
+import SocketServer
+import sqlite3
+class MyTCPHandle(SocketServer.BaseRequestHandler):
+	def add(self,stuid,name,pic):
+
+		return "Add Successfully"
+	def delete(self,stuid):
+		return "Delete Successfully"
+	def modify(self,stuid,name=None,pic=None):
+		return "Modify Successfully"
+	def display(self):
+		pass
+	def error(self):
+		pass
+
+	def handle(self):
+		#self.data=self.request.recv(1024).strip()
+		request=self.request.recv(1024)
+		print "{} wrote:".format(self.client_address[0])
+		print "request is ",request
+		method=request.split(' ')[0]
+		#src=request.split(' ')[1]
+		if method=='add':
+			add(request.split(' ')[1],request.split(' ')[2],request.split(' ')[3])
+		if method=='delete':
+			delete(request.split(' ')[1])
+		if method=='modify':
+			modify(request.split(' ')[1],request.split(' ')[2],request.split(' ')[3])
+		if method=='display':
+			display()
+		else:
+			error()
+
+
+
+		self.request.sendall(request.upper())
+
+if __name__=="__main__":
+	HOST,PORT="localhost",50000
+	server=SocketServer.TCPServer((HOST,PORT),MyTCPHandle)
+	server.serve_forever()
