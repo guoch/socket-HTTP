3771461ac5fa2184eaf03a14cc1dd079cf95b2b3
guoch <linuxwalkers@163.com>
2014-12-07 10:26:43 +0800

    1207

--- client.py
+++ client.py
@@ -1,19 +1,18 @@
 #-*-coding:utf-8-*- 
 import socket
 import sys
-import struct
 reload(sys) 
 sys.setdefaultencoding('utf-8')
 
-address = ('localhost', 50000)
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(address)
+HOST,PORT='localhost',50000
+data=" ".join(sys.argv[1:])
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+try:
+	sock.connect((HOST,PORT))
+	sock.sendall(data+"\n")
+	received=sock.recv(1024)
+finally:
+	sock.close()
 
-messages = ["foobar", "barbaz", "bazquxfad", "Jimmy Carter"]
-
-for s in messages:
-
-    totallen = len(s) 
-    pack1 = struct.pack('>I', totallen) # the first part of the message is length
-    client_socket.sendall(pack1)
-    client_socket.sendall(s)
+print "Sent: {}".format(data)
+print "Received: {}".format(received)
