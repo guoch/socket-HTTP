7bebbbbfab2242ca1657eb64289d780e76dbb7be
guoch <linuxwalkers@163.com>
2014-12-16 14:06:23 +0800

    FInal Project
    最终提交作业

---
+++ src/client/client.py
@@ -0,0 +1,33 @@
+#-*-coding:utf-8-*- 
+import socket
+import sys
+reload(sys) 
+sys.setdefaultencoding('utf-8')
+
+#HOST,PORT='localhost',50000
+PORT=50000
+HOST=raw_input('Please input the server IP: ')
+print "Now connecting to server."
+readme= '''支持指令：\n
+	列出指令 ls :   如 "ls" 列出名单中学生所有信息 \n
+	添加指令 add：  如 "add 112433 Chenghao D:\chenghao.jpg" \n
+	在名单中添加学号为112433 姓名为Chenghao 图片为chenghao.jpg\n
+	删除指令 delete 如 "delete 112433"  在名单中删除学号为112433学生的记录\n
+	修改指令 modify 如 "modify 112433 Guochenghao  "\n
+	把学号为112433的学生姓名改为 Guochenghao\n'''
+readmeGBK=readme.decode('utf-8').encode('gbk')
+print readmeGBK
+while(True):
+	#data=" ".join(sys.argv[1:])
+
+	data=raw_input("Please input your instruction:")
+	sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	try:
+		sock.connect((HOST,PORT))
+		sock.sendall(data)
+		received=sock.recv(1024)
+	finally:
+		sock.close()
+	print "Sent: {}".format(data)
+	print "Received:\n{}".format(received)
+	#if data=="quit":
---
+++ src/server/server.py
@@ -0,0 +1,82 @@
+#-*-coding:utf-8-*- 
+import SocketServer
+import sqlite3
+import sys
+reload(sys) 
+sys.setdefaultencoding('utf-8')
+conn=sqlite3.connect("namelist.db")
+#定义类HTTP协议支持的方法
+
+#在学生名单中添加数据
+def add(stuid,name,picture=None):
+	curadd=conn.cursor()
+	curadd.execute("insert into stu values(?,?,?)",(stuid,name,picture,))
+	conn.commit()
+	return "Add Successfully"
+
+#在学生名单中删除数据
+def delete(stuid):
+	curdel=conn.cursor()
+	curdel.execute("delete from stu where stuid=?",(stuid,))
+	conn.commit()
+	return "Delete Successfully"
+
+#在学生名单中修改数据
+def modify(stuid,name,picture=None):
+	curmod=conn.cursor()
+	curmod.execute("update stu set name=?,picture=? where stuid=?",(name,picture,stuid,))
+	conn.commit()
+	return "Modify Successfully"
+
+def ls():
+	curls=conn.cursor()
+	curls.execute("select stuid,name,picture from stu")
+	result=curls.fetchall()
+	dis=""
+	for line in result:
+		dis=dis+str(line[0])+"\t"+str(line[1])+"\t"+str(line[2])+"\n"
+	return dis
+	#dis=str(result)
+	#return dis
+
+def error():
+	return "The server cannot process your request,Maybe some error"
+
+class GchHttpHandle(SocketServer.BaseRequestHandler):
+	#以下为该类HTTP协议支持的方法
+	def handle(self):
+		#self.data=self.request.recv(1024).strip()
+		request=self.request.recv(1024)
+		print "{} wrote:".format(self.client_address[0])
+		print "request is ",request
+		method=request.split(' ')[0]
+		backmessage=""
+		#src=request.split(' ')[1]
+		print request.split(' ')
+		if method=='add':
+			if len(request.split(' '))<4:
+				backmessage=add(request.split(' ')[1],request.split(' ')[2])
+			else:
+				backmessage=add(request.split(' ')[1],request.split(' ')[2],request.split(' ')[3])
+		elif method=='delete':
+			backmessage=delete(request.split(' ')[1])
+		elif method=='modify':
+			if len(request.split(' '))<4:
+				backmessage=modify(request.split(' ')[1],request.split(' ')[2])
+			else:
+				backmessage=modify(request.split(' ')[1],request.split(' ')[2],request.split(' ')[3])
+		elif method=='ls':
+			backmessage=ls()
+		else:
+			backmessage=error()
+		self.request.sendall(backmessage)
+
+#在主函数中实例化类，制定server端的服务器和端口号，本程序中默认端口号为50000
+if __name__=="__main__":
+	#HOST,PORT="10.131.255.52",50000
+	#HOST=raw_input("Input server eth IP or hostname: ")
+	HOST=''
+	PORT=50000
+	print "The server is now started. Just let clients connect!"
+	server=SocketServer.TCPServer((HOST,PORT),GchHttpHandle)
+	server.serve_forever()
